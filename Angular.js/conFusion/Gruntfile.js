f1501e50ce3d7c2f4535b7bf336d1ea5127495a9
venkattrj <venkatramcool001@gmail.com>
2015-12-24 17:56:26 +0530

    Form Validation

--- Angular.js/conFusion/Gruntfile.js
+++ Angular.js/conFusion/Gruntfile.js
@@ -1,9 +1,16 @@
-// Time how long tasks take. Can help when optimizing build times
+'use strict';
+
+module.exports = function (grunt) {
+	
+	// Time how long tasks take. Can help when optimizing build times
   require('time-grunt')(grunt);
 
   // Automatically load required Grunt tasks
   require('jit-grunt')(grunt);
 
+  require('jit-grunt')(grunt, {
+    useminPrepare: 'grunt-usemin'
+  });
   // Define the configuration for all the tasks
   grunt.initConfig({
           pkg: grunt.file.readJSON('package.json'),
@@ -20,11 +27,104 @@
           'app/scripts/{,*/}*.js'
         ]
       }
-    }
+    },useminPrepare: {
+        html: 'app/menu.html',
+        options: {
+            dest: 'dist'
+        }
+    },
+      // Concat
+    concat: {
+        options: {
+            separator: ';'
+        },
+        // dist configuration is provided by useminPrepare
+        dist: {}
+    },
+      // Uglify
+    uglify: {
+        // dist configuration is provided by useminPrepare
+        dist: {}
+    },
+    cssmin: {
+        dist: {}
+    },
+      // Filerev
+    filerev: {
+        options: {
+            encoding: 'utf8',
+            algorithm: 'md5',
+            length: 20
+        },
+        release: {
+            // filerev:release hashes(md5) all assets (images, js and css )
+            // in dist directory
+            files: [{
+                src: [
+                    'dist/scripts/*.js',
+                    'dist/styles/*.css',
+                ]
+            }]
+        }
+    },
+      // Usemin
+      // Replaces all assets with their revved version in html and css files.
+      // options.assetDirs contains the directories for finding the assets
+      // according to their relative paths
+    usemin: {
+        html: ['dist/*.html'],
+        css: ['dist/styles/*.css'],
+        options: {
+            assetsDirs: ['dist', 'dist/styles']
+        }
+    },
+    copy: {
+      dist: {
+        cwd: 'app',
+        src: [ '**','!styles/**/*.css','!scripts/**/*.js' ],
+        dest: 'dist',
+        expand: true
+      },
+      fonts: {
+          files:[
+              {
+                  //for bootstrap fonts
+                    expand: true,
+                    dot: true,
+                    cwd: 'bower_components/bootstrap/dist',
+                    src: ['fonts/*.*'],
+                    dest: 'dist'
+                }, {
+                    //for font-awesome
+                    expand: true,
+                    dot: true,
+                    cwd: 'bower_components/font-awesome',
+                    src: ['fonts/*.*'],
+                    dest: 'dist'
+                }
+          ]
+        }
+    },
+    clean: {
+        build:{
+            src: [ 'dist/']
+        }
+    },
+    
   });
 
   grunt.registerTask('build', [
-    'jshint'
+    'clean',
+    'jshint',
+    'useminPrepare',
+    'concat',
+    'cssmin',
+    'uglify',
+    'copy',
+    'filerev',
+    'usemin'
   ]);
 
   grunt.registerTask('default',['build']);
+
+};
